aae63c6897c337dfaca40a3a1a10dca939d1878a
Artem <artem@mail>
2022-03-10 16:22:48 +0300

    v1.4

--- defwords.py
+++ defwords.py
@@ -2,45 +2,28 @@ import re
 from collections import Counter
 from prettytable import PrettyTable
 
-def dates():
-    while True:
-        try:
-            x = 1
-            files = input('Введите путь к файлу и имя файла (либо несколько файлов через пробел): ')
-            files_list = files.split(' ') # ['file1', 'file2']
-            data = {}
-            data1 = ''
+def dates(files):
+    x = 1
+    files_list = files.split(' ') # ['file1', 'file2']
+    data = {}
+    data1 = ''
+    
+    for a in files_list:
+        text = open(a).read().lower()
+        data[x] = text
+        data1 += text
+        x += 1
+    return data, data1, files_list
 
-            print('\nВыбрано файлов: ' + str(len(files_list)) + '.')
-            for a in files_list:
-                file = open(a)
-                file1 = open(a)
-                data[x] = file.read().lower()
-                data1 = data1 + file1.read().lower()
-                file.close()
-                x = x + 1
-        except Exception:
-            print('\nНеверный путь и(или) имя файла(файлов)!\n')
-            continue
-        return data, data1, files_list
-
-def start(data, preps_file):
-    while True:
-        h = input('\nВведите цифрой, как бы вы хотели разобрать файлы?\n\n1: Объеденить в один.\n2: Каждый файл отдельно.\n \nВвод "1" или "2": ')
-        if h == str(1):
-            world(data[1], preps_file)
-        elif h == str(2):
-            x = 1
-            for a in data[0]:
-
-                print('\nФайл ' + str(x) + ': ' + data[-1][x-1])
-                world(data[0][x], preps_file)
-                x = x + 1
-
-        else:
-            print('\nНеверный ввод! Попробуйте ещё раз.')
-            continue
-        break
+def start(data, preps_file, sel):
+    if sel == str(1):
+        world(data[1], preps_file)
+    elif sel == str(2):
+        x = 1
+        for a in data[0]:
+            print('\nФайл ' + str(x) + ': ' + data[-1][x-1])
+            world(data[0][x], preps_file)
+            x += 1
 
 def world(data, preps_file):
 
@@ -58,7 +41,6 @@ def world(data, preps_file):
     print('\nВсего слов без предлогов: ' + str(len(words_clear)) + '. ' + 'Из них оригинальных: ' + str(len(orig))+ '.\n')
    
     print('\n' + str(pretty(words_clear, list_c)))
-
 
 def range(list_c):
     while True:
@@ -85,7 +67,7 @@ def pretty(words_clear, list_c):
     for a, b in top15.items(): # [('a', 1), ('b', 2)]
         row[x] = b
         z.add_row([x, a.capitalize(), row[x], round(b / len(words_clear) * 100, 2)])
-        x = x + 1
+        x += 1
 
     z.field_names  =  ["n", "Слово",  "Употр.раз", "Проценты"]
     return z
--- index.py
+++ index.py
@@ -1,14 +1,38 @@
+#!/usr/bin/env python3
+
 from defwords import dates, start
+import os
 
 print('\nВычисляем количество слов в тексте:\n')
+while True:
+    try:
+        files = input('Введите путь к файлу и имя файла (либо несколько файлов через пробел): ') # !
+        data = dates(files)
+        print('\nВыбрано файлов: ' + str(len(data[-1])) + '.')
+        while True:
+            sel = input('\nВведите цифрой, как бы вы хотели разобрать файлы?\n\n1: Объеденить в один.\n2: Каждый файл отдельно.\n \nВвод "1" или "2": ') # !
+            if sel == str(1) or sel == str(2):
+                pass
 
-data = dates()
+            else:
+                print('\nНеверный ввод! Попробуйте ещё раз.')
+                continue
+            break
+    except FileNotFoundError: # !
+        print('\nНеверный путь и(или) имя файла(файлов)!\n')
+        continue
+    break
 
-file1 = open("preps.txt")
-preps_file = file1.read().lower()
-file1.close()
+# file1 = open("preps.txt")
+# preps_file = file1.read().lower()
+# file1.close()
 
-start(data, preps_file)
+with open(os.path.dirname(__file__) + "/preps.txt") as file:
+    preps_file = file.read().lower()
+
+start(data, preps_file, sel)
+
 
 # txt/krovostok_cherepovets.txt
 # /home/user/Code/Files_downloaded_by_AirDroid/krovostok_cherepovets.txt
+# 1 krovostok_cherepovets_copy.txt
